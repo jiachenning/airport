5063293c242425e26ff2b81000d0d5fb1a4dcd91
blues <wangqiangsuperman123@gmail.com>
2013-01-27 18:36:31 +0800

    Add AuthorityTest

---
+++ security/src/test/java/com/wonders/security/entity/AuthorityTest.java
@@ -0,0 +1,16 @@
+package com.wonders.security.entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.wonders.security.test.AbstractJpaDbunitTests;
+
+public class AuthorityTest extends AbstractJpaDbunitTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
