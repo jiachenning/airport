b66a45a1abde09b4ade5fa240b6c8b206a7f72bc
blues <wangqiangsuperman123@gmail.com>
2013-01-27 18:35:30 +0800

    Add RoleTest

---
+++ security/src/test/java/com/wonders/security/entity/RoleTest.java
@@ -0,0 +1,16 @@
+package com.wonders.security.entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.wonders.security.test.AbstractJpaDbunitTests;
+
+public class RoleTest extends AbstractJpaDbunitTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
