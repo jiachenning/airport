ff9d9f544763e4fdc7a1ad19204b9642e2df6d7f
blues <wangqiangsuperman123@gmail.com>
2013-01-29 16:44:55 +0800

    1，加入slf4j-log4j12 依赖，2， 为工程加入方法级别的访问控制

--- security/src/main/java/com/wonders/security/repository/GroupRepository.java
+++ security/src/main/java/com/wonders/security/repository/GroupRepository.java
@@ -3,12 +3,14 @@ package com.wonders.security.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.wonders.framework.repository.MyRepository;
 import com.wonders.security.entity.Group;
 
 public interface GroupRepository extends MyRepository<Group, Long> {
 	
+	@PreAuthorize("hasRole('user')")
 	@Query("from Group g left join fetch g.children where g.parent.id = ?1")
 	List<Group> findByParentId(long parentId);
 
