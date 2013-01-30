b44e777c1e3375b4bbbfff286070ac2451943e2e
blues <wangqiangsuperman123@gmail.com>
2013-01-30 19:55:01 +0800

    去除UserService.loadUserByUsername 上的事务注解

--- security/src/main/java/com/wonders/security/service/UserService.groovy
+++ security/src/main/java/com/wonders/security/service/UserService.groovy
@@ -8,7 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 import com.wonders.security.repository.UserRepository
 
@@ -19,7 +18,6 @@ class UserService implements UserDetailsService {
 	UserRepository userRepository
 
 	@Override
-	@Transactional(readOnly = true)
 	UserDetails loadUserByUsername(String username) {
 		
 //		println "username ===================== $username"
