3fbc63709251c071d32dc3c545f232757fdd3059
blues <wangqiangsuperman123@gmail.com>
2013-02-01 22:12:46 +0800

    修改 UserService.loadUserByUsername方法实现

--- security/src/main/java/com/wonders/security/service/UserService.groovy
+++ security/src/main/java/com/wonders/security/service/UserService.groovy
@@ -2,11 +2,12 @@ package com.wonders.security.service
 
 import javax.inject.Inject
 
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 import com.wonders.security.repository.AccountRepository
 
@@ -17,13 +18,20 @@ class UserService implements UserDetailsService {
 	private AccountRepository accountRepository
 	
 	@Override
+	@Transactional(readOnly = true)
 	UserDetails loadUserByUsername(String username) {
 		
 		def account = accountRepository.findByLoginName(username)
 		if (!account) {
 			throw new UsernameNotFoundException("user not found with username: [$username]!");
 		}
-				
+		
+		println 'load account.roles'
+		println account.roles
+		
+		println 'load account.authorities'
+		println account.authorities
+		
 		new User(account.loginName, account.password, Collections.EMPTY_LIST)
 	}
 	
