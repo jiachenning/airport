2ab898f945bd26eff62faaa654e7c3279f2d8234
blues <wangqiangsuperman123@gmail.com>
2013-02-25 13:37:04 +0800

    dlkfldfkl

--- security/src/test/java/com/wonders/security/repository/RoleRepositoryTest.java
+++ security/src/test/java/com/wonders/security/repository/RoleRepositoryTest.java
@@ -1,19 +1,40 @@
 package com.wonders.security.repository;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Iterator;
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.junit.Test;
 
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.wonders.security.entity.Role;
 import com.wonders.security.test.AbstractSpringTests;
 
+@DatabaseSetup("RoleRepositoryTest-DatabaseSetup.xml")
 public class RoleRepositoryTest extends AbstractSpringTests {
 	
 	@Inject
 	private RoleRepository roleRepository;
+	
+	@Test
+	public void testCount() {
+		
+		long count = roleRepository.count();
+		assertEquals(3, count);
+	}
 
 	@Test
 	public void testFindByAccountId() {
-		roleRepository.findByAccountId(0);
+		
+		List<Role> roles = roleRepository.findByAccountId(1);
+		Iterator<Role> iter = roles.iterator();
+		
+		assertEquals(2, roles.size());
+		assertEquals(1, (long) iter.next().getId());
+		assertEquals(3, (long) iter.next().getId());
 	}
 
 }
--- security/src/test/java/com/wonders/security/test/AbstractSpringTests.java
+++ security/src/test/java/com/wonders/security/test/AbstractSpringTests.java
@@ -3,13 +3,22 @@ package com.wonders.security.test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.wonders.security.config.AppConfig;
 
 @ContextConfiguration(classes = AppConfig.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles("test")
+@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class,
+    TransactionalTestExecutionListener.class,
+    DbUnitTestExecutionListener.class })
 public abstract class AbstractSpringTests {
 
 }
