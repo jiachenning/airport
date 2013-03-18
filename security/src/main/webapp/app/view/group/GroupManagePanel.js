7536da8cc3a04e2858b1837d546c108451c11623
zhuhaijian <zhuhaijian@wondersgroup.com>
2013-03-18 10:20:40 +0800

    对代码进行格式化

--- security/src/main/webapp/app/view/group/GroupManagePanel.js
+++ security/src/main/webapp/app/view/group/GroupManagePanel.js
@@ -1,40 +1,38 @@
 Ext.define('security.view.group.GroupManagePanel', {
-	extend: 'Ext.panel.Panel',
-	alias: 'widget.groupmanagepanel',
-	requires: [
-	    'security.view.group.GroupTree', 
-	    'security.view.account.AccountGrid',
-	    'security.view.group.RootGroupGrid'
-	],
-	
-	title: '部门管理',
-	closable: true,
-	layout: {
-		type: 'hbox',
-		align: 'stretch'
+	extend : 'Ext.panel.Panel',
+	alias : 'widget.groupmanagepanel',
+	requires : [ 'security.view.group.GroupTree',
+			'security.view.account.AccountGrid',
+			'security.view.group.RootGroupGrid' ],
+
+	title : '部门管理',
+	closable : true,
+	layout : {
+		type : 'hbox',
+		align : 'stretch'
 	},
-	bodyPadding: 1,
-	
-	initComponent: function(arguments) {
-		
+	bodyPadding : 1,
+
+	initComponent : function(arguments) {
+
 		var me = this;
-		
+
 		Ext.applyIf(me, {
-			items: [{
-				xtype: 'rootgroupgrid',
-				hasToolbar: this.hasToolbar,
-			    operable: this.operable,
-			    pagable: this.pagable,
-				width: 350
-			},{
-				xtype: 'grouptree',
-				flex: 1
-			},{
-				xtype: 'accountgrid',
-				flex: 1
-			}]
+			items : [ {
+				xtype : 'rootgroupgrid',
+				hasToolbar : this.hasToolbar,
+				operable : this.operable,
+				pagable : this.pagable,
+				width : 350
+			}, {
+				xtype : 'grouptree',
+				flex : 1
+			}, {
+				xtype : 'accountgrid',
+				flex : 1
+			} ]
 		});
-		
+
 		me.callParent(arguments);
 	}
 });
