82e766f55505feed9598c6301c097fd63cb71e33
zhuhaijian <zhuhaijian@wondersgroup.com>
2013-10-10 11:45:16 +0800

    解决级联选择Bug

--- security/src/main/webapp/app/view/authority/AuthorityCheckedTree.js
+++ security/src/main/webapp/app/view/authority/AuthorityCheckedTree.js
@@ -5,7 +5,39 @@ Ext.define('security.view.authority.AuthorityCheckedTree', {
     useArrows: true,
     autoScroll: true, 
     rootVisible: false,
-	initComponent : function(arguments) {
+    setChildNode:function(childNodes,checked){
+        var node;
+        for(var i=0;i<childNodes.length;i++){
+         node= childNodes[i];
+         node.set('checked', checked);  
+         if(node.childNodes.length>0){
+         this.setChildNode(node.childNodes, checked); 
+         }
+        }
+   },
+   setParentNode:function(node,checked){
+     node.set('checked', checked);  
+     if(node.parentNode!=null){
+     this.setParentNode(node.parentNode, checked); 
+     }
+      
+   },
+   listeners:{
+    checkchange:function(node, checked,eOpts ){
+      if(checked){
+        if(node.parentNode!=null){
+         this.setParentNode(node.parentNode,checked);
+        }
+       node.expand();
+      }else{
+      // node.collapse();
+      }
+      if(node.childNodes.length>0){
+       this.setChildNode(node.childNodes,checked);
+      }
+    }    
+   },
+   initComponent : function(arguments) {
 		var me = this;
 
 		Ext.applyIf(me, {
